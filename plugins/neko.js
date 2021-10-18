54c0e2f52d700cc554fde16fb59e33f79ecb950e
pmdrive <72849497+pmdrive@users.noreply.github.com>
2021-10-18 03:02:28 +0000

    Update neko.js

--- plugins/neko.js
+++ plugins/neko.js
@@ -2,10 +2,10 @@
 
 let fetch = require('node-fetch')
 let handler = async(m, { conn }) => {
-  let res = await fetch('https://api.waifu.pics/sfw/neko')
+  let res = await fetch('https://bot.rave.my.id')
   if (!res.ok) throw await res.text()
   let json = await res.json()
-  if (!json.url) throw 'Error!'
+  if (!json.url) throw 'Astagfirullah Inget Dosa!'
   conn.sendFile(m.chat, json.url, '', 'Nyaa', m)
 }
 handler.help = ['neko']
