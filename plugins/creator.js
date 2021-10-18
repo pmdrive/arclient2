c541039b5b528861e060fa6d84a81f234cc5059c
pmdrive <72849497+pmdrive@users.noreply.github.com>
2021-10-18 02:50:09 +0000

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,7 +1,7 @@
 function handler(m) {
   // Ini cuma contoh, jgn di uncomment -_-
-  // F this.sendContact(m.chat, '62815158600891', 'Nurutomo', m)
-  this.sendContact(m.chat, '6281334177340', 'Nurutomo', m)
+  // F this.sendContact(m.chat, '62895610720367', 'Akbar', m)
+  this.sendContact(m.chat, '62895610720367', 'Akbar', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
